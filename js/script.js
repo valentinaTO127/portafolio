2e7a15bb038ede6a048ab8fda42669e4282fd83c
Valentina Torres <vtorresoro@unbosque.edu.co>
2026-04-10 23:19:56 -0500

    Animations
    Animatios on skill bars and number were added in all three html files, backgrounds animations (the circles) are in process (only index file)

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@ let menuButton = document.getElementById("menu-img");
 let listaNav = document.getElementById("lista-nav");
 let nav = document.getElementById("nav");
 
+
+const barras = document.querySelectorAll(".animtaion-bar");
+const curtains = document.querySelectorAll(".curtain");
+
 //Defines the status of the nav in smartphones
 let active = false;
+
+let waitTime = 0;
 
 menuButton.addEventListener("click", function(event) {
     active == false ? active = true : active = false;
@@ -37,3 +43,33 @@ function change(status) {
     }
             
 }
+
+
+
+
+//noticing at what time the element becomes visible fpr the user (IntersectionObserver is a native API in Chrome, Edge, Firefox and Safari, there's no need to install a library)
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+    }
+  });
+});
+
+
+barras.forEach(barra => {
+    observer.observe(barra);
+});
+
+//timer (not working completely)
+for (let i = 0; i < curtains.length; i++) {
+    setTimeout(() => {
+        observer.observe(curtains[i]);
+    }, i*500);
+
+}
+
+
+
+
