18e2c5ef0be8cd8739c1f6aae49bcee2687aeb5b
Valentina Torres <vtorresoro@unbosque.edu.co>
2026-04-13 08:45:53 -0500

    Mejora de una animacion
    Mejora la animacion de los circulos en la seccion de cifras y el espacion entre elemento en celular

--- js/script.js
+++ js/script.js
@@ -6,11 +6,12 @@ let nav = document.getElementById("nav");
 
 const barras = document.querySelectorAll(".animtaion-bar");
 const curtains = document.querySelectorAll(".curtain");
+const circles = document.querySelectorAll(".static");
 
 //Defines the status of the nav in smartphones
 let active = false;
 
-let waitTime = 0;
+let waitTime = false;
 
 menuButton.addEventListener("click", function(event) {
     active == false ? active = true : active = false;
@@ -50,10 +51,13 @@ function change(status) {
 //noticing at what time the element becomes visible fpr the user (IntersectionObserver is a native API in Chrome, Edge, Firefox and Safari, there's no need to install a library)
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
+ 
     if (entry.isIntersecting) {
+        console.log(waitTime);
         entry.target.classList.add("visible");
         observer.unobserve(entry.target);
     }
+    
   });
 });
 
@@ -62,13 +66,18 @@ barras.forEach(barra => {
     observer.observe(barra);
 });
 
+circles.forEach(circle => {
+    observer.observe(circle);
+});
+
 //timer (not working completely)
 for (let i = 0; i < curtains.length; i++) {
     setTimeout(() => {
         observer.observe(curtains[i]);
     }, i*500);
-
 }
+
+
 
 
 
